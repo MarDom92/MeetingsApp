181b9aa5e098dc6418f8156239f4bc2dbe2b5a20
MarDom92 <marcin.domanski@gmail.com>
2022-04-18 15:27:15 +0200

    init commit

---
+++ src/main/java/pl/mardom92/MeetingsApp/MeetingsAppApplication.java
@@ -0,0 +1,13 @@
+package pl.mardom92.MeetingsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mardom92/MeetingsApp/MeetingsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mardom92.MeetingsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
