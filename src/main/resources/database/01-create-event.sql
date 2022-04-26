9803e1b5a9675022a5243fee844b00e18da94cd5
MarDom92 <marcin.domanski92@gmail.com>
2022-04-26 21:42:46 +0200

    sql scripts for create table event and insert data into db

--- src/main/resources/database/01-create-event.sql
+++ src/main/resources/database/01-create-event.sql
@@ -1,9 +1,10 @@
 --liquibase formatted sql
---changeset mardom:1
+--changeSet mardom:1
+
 CREATE TABLE EVENT
 (
-    id      SERIAL PRIMARY KEY,
-    title   VARCHAR(400)  NOT NULL,
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(400)  NOT NULL,
     description VARCHAR(2000) NULL,
     place VARCHAR(400) NULL,
     created_date timestamp,
---
+++ src/main/resources/database/02-add-event-data.sql
@@ -0,0 +1,32 @@
+--liquibase formatted sql
+--changeSet mardom:2
+
+insert into event(title, description, place, created_date, updated_date, start_date, end_date)
+values ('Event 1', 'Description 1', 'Warszawa', '2019-01-01T13:14:05.562673800', '2020-01-01T13:14:05.574458300', '2021-01-01T13:14:05.602757600', '2022-01-01T13:14:05.616423900');
+
+insert into event(title, description, place, created_date, updated_date, start_date, end_date)
+values ('Event 2', 'Description 2', 'Poznań', '2019-02-01T13:14:05.562673800', '2020-02-01T13:14:05.574458300', '2021-02-01T13:14:05.602757600', '2022-02-01T13:14:05.616423900');
+
+insert into event(title, description, place, created_date, updated_date, start_date, end_date)
+values ('Event 3', 'Description 3', 'Kraków', '2019-03-01T13:14:05.562673800', '2020-03-01T13:14:05.574458300', '2021-03-01T13:14:05.602757600', '2022-03-01T13:14:05.616423900');
+
+insert into event(title, description, place, created_date, updated_date, start_date, end_date)
+values ('Event 4', 'Description 4', 'Gdańsk', '2019-04-01T13:14:05.562673800', '2020-04-01T13:14:05.574458300', '2021-04-01T13:14:05.602757600', '2022-04-01T13:14:05.616423900');
+
+insert into event(title, description, place, created_date, updated_date, start_date, end_date)
+values ('Event 5', 'Description 5', 'Szczecin', '2019-05-01T13:14:05.562673800', '2020-05-01T13:14:05.574458300', '2021-05-01T13:14:05.602757600', '2022-05-01T13:14:05.616423900');
+
+insert into event(title, description, place, created_date, updated_date, start_date, end_date)
+values ('Event 6', 'Description 6', 'Wrocław', '2019-06-01T13:14:05.562673800', '2020-06-01T13:14:05.574458300', '2021-06-01T13:14:05.602757600', '2022-06-01T13:14:05.616423900');
+
+insert into event(title, description, place, created_date, updated_date, start_date, end_date)
+values ('Event 7', 'Description 7', 'Łódź', '2019-07-01T13:14:05.562673800', '2020-07-01T13:14:05.574458300', '2021-07-01T13:14:05.602757600', '2022-07-01T13:14:05.616423900');
+
+insert into event(title, description, place, created_date, updated_date, start_date, end_date)
+values ('Event 8', 'Description 8', 'Lublin', '2019-08-01T13:14:05.562673800', '2020-08-01T13:14:05.574458300', '2021-08-01T13:14:05.602757600', '2022-08-01T13:14:05.616423900');
+
+insert into event(title, description, place, created_date, updated_date, start_date, end_date)
+values ('Event 9', 'Description 9', 'Sosnowiec', '2019-09-01T13:14:05.562673800', '2020-09-01T13:14:05.574458300', '2021-09-01T13:14:05.602757600', '2022-09-01T13:14:05.616423900');
+
+insert into event(title, description, place, created_date, updated_date, start_date, end_date)
+values ('Event 10', 'Description 1', 'Radom', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300', '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900');
