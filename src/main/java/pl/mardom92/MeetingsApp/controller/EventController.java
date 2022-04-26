2dff30ca8ca3c84d626c8f41d6636646e49b12e8
Marcin Domanski <marcin.domanski92@gmail.com>
2022-04-26 13:46:27 +0200

    add, edit and delete methods for event api

--- src/main/java/pl/mardom92/MeetingsApp/controller/EventController.java
+++ src/main/java/pl/mardom92/MeetingsApp/controller/EventController.java
@@ -1,9 +1,7 @@
 package pl.mardom92.MeetingsApp.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.mardom92.MeetingsApp.model.Event;
 import pl.mardom92.MeetingsApp.service.EventService;
 
@@ -11,17 +9,33 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(path = "/events")
 public class EventController {
 
     private final EventService eventService;
 
-    @GetMapping("/events")
+    @GetMapping("/")
     public List<Event> getEvents() {
         return eventService.getEvents();
     }
 
-    @GetMapping("/events/{id}")
+    @GetMapping("/{id}")
     public Event getSingleEvents(@PathVariable long id) {
         return eventService.getSingleEvent(id);
+    }
+
+    @PostMapping("/")
+    public Event addEvent(@RequestBody Event event) {
+        return eventService.addEvent(event);
+    }
+
+    @PutMapping("/")
+    public Event editEvent(@RequestBody Event event) {
+        return eventService.editEvent(event);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEvent(@PathVariable long id) {
+        eventService.deleteEvent(id);
     }
 }
--- src/main/java/pl/mardom92/MeetingsApp/service/EventService.java
+++ src/main/java/pl/mardom92/MeetingsApp/service/EventService.java
@@ -21,4 +21,26 @@ public class EventService {
         return eventRepository.findById(id)
                 .orElseThrow();
     }
+
+    public Event addEvent(Event event) {
+        return eventRepository.save(event);
+    }
+
+    public Event editEvent(Event event) {
+
+        Event eventEdited = eventRepository.findById(event.getId()).orElseThrow();
+
+        eventEdited.setTitle(eventEdited.getTitle());
+        eventEdited.setDescription(eventEdited.getDescription());
+        eventEdited.setPlace(eventEdited.getPlace());
+        eventEdited.setUpdatedDate(eventEdited.getUpdatedDate());
+        eventEdited.setStartDate(eventEdited.getStartDate());
+        eventEdited.setEndDate(eventEdited.getEndDate());
+
+        return eventRepository.save(event);
+    }
+
+    public void deleteEvent(long id) {
+        eventRepository.deleteById(id);
+    }
 }
