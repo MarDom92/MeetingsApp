b5c36473a04b46bf5fc705febf1d144087024395
MarDom92 <marcin.domanski92@gmail.com>
2022-04-27 02:10:53 +0200

    add EventServiceHelper

---
+++ src/main/java/pl/mardom92/MeetingsApp/service/EventServiceHelper.java
@@ -0,0 +1,53 @@
+package pl.mardom92.MeetingsApp.service;
+
+import org.springframework.stereotype.Component;
+import pl.mardom92.MeetingsApp.model.dto.EventDto;
+import pl.mardom92.MeetingsApp.model.entity.Event;
+import pl.mardom92.MeetingsApp.model.exception.EventError;
+import pl.mardom92.MeetingsApp.model.exception.EventException;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+@Component
+public class EventServiceHelper {
+
+    protected void checkEmptyList(List<Event> events) {
+
+        if (events.isEmpty()) {
+            throw new EventException(EventError.EVENT_EMPTY_LIST);
+        }
+    }
+
+    protected void checkEventValues(EventDto eventDto) {
+
+        if (eventDto.getTitle().trim().equals("")) {
+            throw new EventException(EventError.EVENT_WRONG_FIELD_VALUE);
+        }
+
+        if (eventDto.getDescription().trim().equals("")) {
+            throw new EventException(EventError.EVENT_WRONG_FIELD_VALUE);
+        }
+
+        if (eventDto.getPlace().trim().equals("")) {
+            throw new EventException(EventError.EVENT_WRONG_FIELD_VALUE);
+        }
+
+        SimpleDateFormat format = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
+
+        try {
+            format.parse(eventDto.getStartDate().toString());
+        }
+        catch (ParseException e) {
+            new EventException(EventError.EVENT_WRONG_DATE_FORMAT);
+        }
+
+        try {
+            format.parse(eventDto.getEndDate().toString());
+        }
+        catch (ParseException e) {
+            new EventException(EventError.EVENT_WRONG_DATE_FORMAT);
+        }
+    }
+}
