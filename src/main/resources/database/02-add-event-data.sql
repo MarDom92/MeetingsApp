c75a58128ef7c04d9d7479dd22d6e02d62429037
Marcin Domanski <marcin.domanski92@gmail.com>
2022-04-28 12:38:50 +0200

    refactor sql add data files

--- src/main/resources/database/02-add-event-data.sql
+++ src/main/resources/database/02-add-event-data.sql
@@ -2,61 +2,43 @@
 --changeSet mardom:2
 
 insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 1', 'Description 1', 'Warszawa', '2019-01-01T13:14:05.562673800', '2020-01-01T13:14:05.574458300', '2021-01-01T13:14:05.602757600', '2022-01-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 2', 'Description 2', 'Poznań', '2019-02-01T13:14:05.562673800', '2020-02-01T13:14:05.574458300', '2021-02-01T13:14:05.602757600', '2022-02-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 3', 'Description 3', 'Kraków', '2019-03-01T13:14:05.562673800', '2020-03-01T13:14:05.574458300', '2021-03-01T13:14:05.602757600', '2022-03-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 4', 'Description 4', 'Gdańsk', '2019-04-01T13:14:05.562673800', '2020-04-01T13:14:05.574458300', '2021-04-01T13:14:05.602757600', '2022-04-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 5', 'Description 5', 'Szczecin', '2019-05-01T13:14:05.562673800', '2020-05-01T13:14:05.574458300', '2021-05-01T13:14:05.602757600', '2022-05-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 6', 'Description 6', 'Wrocław', '2019-06-01T13:14:05.562673800', '2020-06-01T13:14:05.574458300', '2021-06-01T13:14:05.602757600', '2022-06-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 7', 'Description 7', 'Łódź', '2019-07-01T13:14:05.562673800', '2020-07-01T13:14:05.574458300', '2021-07-01T13:14:05.602757600', '2022-07-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 8', 'Description 8', 'Lublin', '2019-08-01T13:14:05.562673800', '2020-08-01T13:14:05.574458300', '2021-08-01T13:14:05.602757600', '2022-08-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 9', 'Description 9', 'Sosnowiec', '2019-09-01T13:14:05.562673800', '2020-09-01T13:14:05.574458300', '2021-09-01T13:14:05.602757600', '2022-09-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 10', 'Description 10', 'Radom', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300', '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 11', 'Description 11', 'Olsztyn', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300', '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 12', 'Description 12', 'Białystok', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300', '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 13', 'Description 13', 'Toruń', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300', '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 14', 'Description 14', 'Sopot', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300', '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 15', 'Description 15', 'Gdynia', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300', '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 16', 'Description 16', 'Otwock', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300', '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 17', 'Description 17', 'Sulejówek', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300', '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 18', 'Description 18', 'Pruszków', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300', '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 19', 'Description 19', 'Wołomin', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300', '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900');
-
-insert into event(title, description, place, created_date, updated_date, start_date, end_date)
-values ('Event 20', 'Description 20', 'Świebodzin', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300', '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900');
+values ('Event 1', 'Description 1', 'Warszawa', '2019-01-01T13:14:05.562673800', '2020-01-01T13:14:05.574458300',
+        '2021-01-01T13:14:05.602757600', '2022-01-01T13:14:05.616423900'),
+       ('Event 2', 'Description 2', 'Poznań', '2019-02-01T13:14:05.562673800', '2020-02-01T13:14:05.574458300',
+        '2021-02-01T13:14:05.602757600', '2022-02-01T13:14:05.616423900'),
+       ('Event 3', 'Description 3', 'Kraków', '2019-03-01T13:14:05.562673800', '2020-03-01T13:14:05.574458300',
+        '2021-03-01T13:14:05.602757600', '2022-03-01T13:14:05.616423900'),
+       ('Event 4', 'Description 4', 'Gdańsk', '2019-04-01T13:14:05.562673800', '2020-04-01T13:14:05.574458300',
+        '2021-04-01T13:14:05.602757600', '2022-04-01T13:14:05.616423900'),
+       ('Event 5', 'Description 5', 'Szczecin', '2019-05-01T13:14:05.562673800', '2020-05-01T13:14:05.574458300',
+        '2021-05-01T13:14:05.602757600', '2022-05-01T13:14:05.616423900'),
+       ('Event 6', 'Description 6', 'Wrocław', '2019-06-01T13:14:05.562673800', '2020-06-01T13:14:05.574458300',
+        '2021-06-01T13:14:05.602757600', '2022-06-01T13:14:05.616423900'),
+       ('Event 7', 'Description 7', 'Łódź', '2019-07-01T13:14:05.562673800', '2020-07-01T13:14:05.574458300',
+        '2021-07-01T13:14:05.602757600', '2022-07-01T13:14:05.616423900'),
+       ('Event 8', 'Description 8', 'Lublin', '2019-08-01T13:14:05.562673800', '2020-08-01T13:14:05.574458300',
+        '2021-08-01T13:14:05.602757600', '2022-08-01T13:14:05.616423900'),
+       ('Event 9', 'Description 9', 'Sosnowiec', '2019-09-01T13:14:05.562673800', '2020-09-01T13:14:05.574458300',
+        '2021-09-01T13:14:05.602757600', '2022-09-01T13:14:05.616423900'),
+       ('Event 10', 'Description 10', 'Radom', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300',
+        '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900'),
+       ('Event 11', 'Description 11', 'Olsztyn', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300',
+        '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900'),
+       ('Event 12', 'Description 12', 'Białystok', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300',
+        '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900'),
+       ('Event 13', 'Description 13', 'Toruń', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300',
+        '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900'),
+       ('Event 14', 'Description 14', 'Sopot', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300',
+        '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900'),
+       ('Event 15', 'Description 15', 'Gdynia', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300',
+        '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900'),
+       ('Event 16', 'Description 16', 'Otwock', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300',
+        '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900'),
+       ('Event 17', 'Description 17', 'Sulejówek', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300',
+        '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900'),
+       ('Event 18', 'Description 18', 'Pruszków', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300',
+        '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900'),
+       ('Event 19', 'Description 19', 'Wołomin', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300',
+        '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900'),
+       ('Event 20', 'Description 20', 'Świebodzin', '2019-01-01T13:14:05.562673800', '2020-10-01T13:14:05.574458300',
+        '2021-10-01T13:14:05.602757600', '2022-10-01T13:14:05.616423900');
